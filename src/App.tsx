870e7bd06b7d4fb5a1c02af6b211511cf5553ae5
Nayan Jyoti Das <dastms@gmail.com>
2022-01-20 10:02:00 +0530

    Make dashboard responsive for small devices

--- src/App.tsx
+++ src/App.tsx
@@ -11,37 +11,37 @@ import { ReactComponent as SettingsLogo } from "./assets/settingsLogo.svg";
 
 function App() {
   return (
-    <div className="flex">
-      <div className="h-screen w-88px flex flex-col justify-start">
+    <div className="flex flex-wrap sm:flow-row flex-col-reverse h-screen sm:items-start items-end border-2 ">
+      <div className="border-2 w-screen sm:w-88px h-fit sm:h-screen  flex flex-row sm:flex-col py-2 sm:py-0 justify-items-center items-center overflow-auto">
         {/* <div className="mt-[40px] text-center font-extrabold text-iconDColor text-xl underline underline-offset-2 decoration-2">
           D
         </div>
         <div className="text-center text-duidRed font-semibold text-xs">
           Duid
         </div> */}
-        <div className="mt-[40px] h-fit grid justify-items-center">
+        <div className="hidden sm:block mt-[20px] sm:mt-[40px] h-fit grid justify-items-center">
           <DuidLogo />
         </div>
-        <div className="mt-[120px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
+        <div className="sm:mt-[100px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
           <HomeRouteLogo />
         </div>
-        <div className="mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
+        <div className="sm:mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
           <WalletLogo />
         </div>
-        <div className="mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
+        <div className="sm:mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
           <CalendarLogo />
         </div>
-        <div className="mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
+        <div className="sm:mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
           <StatsLogo />
         </div>
-        <div className="mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
+        <div className="sm:mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
           <MessagesLogo />
         </div>
-        <div className="mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
+        <div className="sm:mt-[20px] h-fit rounded-md w-fit mx-auto grid p-2 justify-items-center transition ease-in-out hover:scale-150">
           <SettingsLogo />
         </div>
       </div>
-      <div className="grow h-screen border-2 bg-creamyWhite"></div>
+      <div className="grow sm:h-screen w-screen border-2 bg-creamyWhite"></div>
     </div>
   );
 }
